832c7cc4bff07b1f1794ee81c52785afde3ba561
Eason-F <eason.ycfeng@outlook.com>
2026-09-12 14:38:21 +0800

    fix: adjust for 2mm ir

--- src/main.cpp
+++ src/main.cpp
@@ -115,7 +115,7 @@ float calculateSignalStrength(
       100.0F;
 
   constexpr float SIGNAL_MIN = 0.0F;
-  constexpr float SIGNAL_MAX = 33.0F;
+  constexpr float SIGNAL_MAX = 40.0F;
 
   strength =
       ((strength - SIGNAL_MIN) /
